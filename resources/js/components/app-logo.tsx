2aebd31c0c56e0304ad55f33967fd49dc796cd40
Ui Create Design <uicreatedesign@gmail.com>
2025-12-13 18:27:17 +0530

    ADD : Build added

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -10,7 +10,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold hidden">
+                <span className="mb-0.5 truncate leading-tight font-semibold">
                     {appSettings?.app_name || 'Boutique System'}
                 </span>
             </div>
